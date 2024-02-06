a3c9fd334eebb5cd669756e7fb9daf8b4c8c7bed
evkondr <kondratiev.evg@gmail.com>
2024-02-07 00:08:23 +0300

    refactor: add new middleware with image compression

--- src/routes/menuRoute.ts
+++ src/routes/menuRoute.ts
@@ -1,12 +1,18 @@
-import Router from 'express';
+import Router, { Request, Response } from 'express';
 // import secureRote from '../middleware/secureRoute';
 import MenuController from '../controllers/menuController';
 import imageUpload from '../middleware/imageUpload';
 import authMiddleware from '../middleware/authMiddleware';
 import { UserRole } from '../../types';
 import PromoMenuController from '../controllers/promoMenuController';
+import compressImageMiddleware from '../middleware/compressImageMiddleware';
 
 const router = Router();
+router.post('/test', compressImageMiddleware, (req: Request, res: Response) => {
+  res.json({
+    image: req.file?.filename,
+  });
+});
 // PROMO
 router.get('/promo', PromoMenuController.getAllPromos);
 router.post('/promo', authMiddleware([UserRole.ADMIN, UserRole.EDITOR]), PromoMenuController.addMenuItemToPromo);
