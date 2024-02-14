67d488e120ef6b790bec7d8472080d2309dba609
evkondr <kondratiev.evg@gmail.com>
2024-02-14 23:42:33 +0300

    feat: add auth on orders

--- src/routes/menuRoute.ts
+++ src/routes/menuRoute.ts
@@ -13,7 +13,7 @@ router.get('/promo', PromoMenuController.getAllPromos);
 router.post('/promo', authMiddleware([UserRole.ADMIN, UserRole.EDITOR]), PromoMenuController.addMenuItemToPromo);
 router.delete('/promo/', authMiddleware([UserRole.ADMIN, UserRole.EDITOR]), PromoMenuController.deleteMenuItemFromPromo);
 // Main menu items
-router.get('/', MenuController.getMenuItems);
+router.get('/', authMiddleware([UserRole.ADMIN, UserRole.EDITOR]), MenuController.getMenuItems);
 router.get('/:id', authMiddleware([UserRole.ADMIN, UserRole.EDITOR]), MenuController.getMenuItemByID);
 router.post('/', compressImageMiddleware, authMiddleware([UserRole.ADMIN, UserRole.EDITOR]), MenuController.createMenuItem);
 router.patch('/:id', authMiddleware([UserRole.ADMIN, UserRole.EDITOR]), imageUpload, MenuController.updateMenuItem);
