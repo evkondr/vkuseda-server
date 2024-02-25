371c2e1e315d267cf04dc2370ee227b943db614e
evkondr <kondratiev.evg@gmail.com>
2024-02-25 23:14:08 +0300

    feat: add secureRoute to mailRoute

--- src/routes/mailRoute.ts
+++ src/routes/mailRoute.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import MailControeller from '../controllers/mailController';
+import secureRote from '../middleware/secureRoute';
 
 const router = Router();
 
-router.post('/send-email', MailControeller.sendEmail);
+router.post('/send-email', secureRote, MailControeller.sendEmail);
 
 export default router;
