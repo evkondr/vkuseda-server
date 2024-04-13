9535976c5d5c7c6f94373072d8edd6e406e49293
evkondr <kondratiev.evg@gmail.com>
2024-04-13 09:42:16 +0300

    fix: route name for add-menu-items

--- src/routes/daysRoute.ts
+++ src/routes/daysRoute.ts
@@ -10,7 +10,7 @@ router.get('/:id', DaysControlle.getDayById);
 router.post('/', authMiddleware([UserRole.ADMIN, UserRole.EDITOR]), DaysControlle.createDay);
 router.delete('/:id', authMiddleware([UserRole.ADMIN, UserRole.EDITOR]), DaysControlle.deleteDay);
 
-router.post('/menu-items', authMiddleware([UserRole.ADMIN, UserRole.EDITOR]), DaysControlle.addMenuItem);
+router.post('/add-menu-items', authMiddleware([UserRole.ADMIN, UserRole.EDITOR]), DaysControlle.addMenuItem);
 router.post('/delete-menu-item', authMiddleware([UserRole.ADMIN, UserRole.EDITOR]), DaysControlle.delMenuItem);
 
 export default router;
